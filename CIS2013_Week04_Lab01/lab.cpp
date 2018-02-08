bb1274c13801c953eb4a5171ee3991db9d89be23
Daniel Novotny <svc_okc_bt204@ad.osuokc.edu>
2018-02-07 20:09:35 -0600

    lab

---
+++ CIS2013_Week04_Lab01/lab.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printstuff(){
+	cout << "printing stuff" << endl;
+}
+
+
+int main() {
+	for (int i = 0; i < 10; i++) {
+		printstuff();
+	}
+
+	return 0;
+}
---
+++ lab.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+char a;
+int large_number = 0;
+void printStuff(int);
+void printstuff(int,int);
+
+
+
+
+int main() {
+	for (int i = 0; i < 10;i++) {
+		large_number = large_number + i;
+		printStuff(i,large_number);
+		cout << "Printing with only one argument" << endl;
+		printStuff(i);
+	}
+	
+
+
+	cin >>a;
+	return 0;
+}
+void printStuff(int num) {
+	cout << "printing stuff" << (num * 5) << endl;
+	
+}
+
+void printStuff(int num, int total) {
+	cout << "printing stuff" << (num * 5) << "and total " << total << endl;
+	
+}
+
+//CIS2013_Week04_Homework1
+//CIS2013_Week04_Homework2
